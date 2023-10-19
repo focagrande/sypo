c716c29d8fd3b4bd1bdc5ba49a7cbd3c33279faa
Jacek Śpiewak <jacek.spiewak@gmail.com>
2023-10-19 13:50:37 +0000

    modern python

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from __future__ import print_function
-
 from sypo.population import Population
 
 if __name__ == '__main__':
-    
+
     p = Population(5, 10)
     p.populate()
 
     for k in p.population:
-        print('{} => {}'.format(k, k.decode()))
+        print(f"{k} => {k.decode()}")
